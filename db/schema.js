abe54da2057acf40a89163ec78e4cfa3ab2e51fd
Ross Davis <rossjdavis@gmail.com>
2018-01-05 12:03:15 -0500

    MsgSchema starter // commented

--- db/schema.js
+++ db/schema.js
@@ -39,16 +39,24 @@ const ReviewSchema = new Schema({
   comment: String
 })
 
+// const MsgSchema = new Schema({
+//   author: { type: ObjectId, ref: 'Human' },
+//   recipient: { type: ObjectId, ref: 'Human' },
+//   text: String
+// })
+
 const Canine = mongoose.model('Canine', CanineSchema)
 const Human = mongoose.model('Human', HumanSchema)
 // const Venue = mongoose.model('Venue', VenueSchema)
 const Event = mongoose.model('Event', EventSchema)
 const Review = mongoose.model('Review', ReviewSchema)
+const Msg = mongoose.model('Msg', MsgSchema)
 
 module.exports = {
   Canine,
   Human,
   // Venue,
   Event,
-  Review
+  Review,
+  Msg
 }
